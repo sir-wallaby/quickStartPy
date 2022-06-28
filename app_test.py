2ffe56128b1fa561be767f9e42ed4b2b8b5b95f1
wtaylor <wesley.taylor303@gmail.com>
2022-06-27 22:46:02 -0400

    minor message updates

--- app_test.py
+++ app_test.py
@@ -9,4 +9,17 @@ client = discord.Client()
 async def on_ready():
     print('we have logged in as {0.user}'.format(client))
 
+@client.event
+async def on_message(message):
+    if message.content.startswith('apex'):
+        channel = message.channel
+        await channel.send('I guess')
+
+        def check(m):
+            return m.content == 'hello' and m.channel == channel
+
+        msg = await client.wait_for('message', check=check)
+        await channel.send(f'Hello {msg.author}!')
+
+
 client.run(token)
